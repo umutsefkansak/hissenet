686d0cdb4fa946d7e012209708f19fa8583f9054
umutsefkansak <umut.sefkan4@gmail.com>
2025-07-28 17:01:15 +0300

    chore: initialize project structure with empty Spring Boot and React apps

---
+++ backend/hissenet/src/main/java/com/infina/hissenet/HissenetApplication.java
@@ -0,0 +1,13 @@
+package com.infina.hissenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HissenetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HissenetApplication.class, args);
+	}
+
+}
---
+++ backend/hissenet/src/test/java/com/infina/hissenet/HissenetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infina.hissenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HissenetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

289028036ba05072062f35a1448c63ec842b768a
umutsefkansak <umut.sefkan4@gmail.com>
2025-07-30 02:44:29 +0300

    feat: add generic service structure for reusable CRUD operations

---
+++ backend/hissenet/src/main/java/com/infina/hissenet/utils/GenericServiceImpl.java
@@ -0,0 +1,48 @@
+package com.infina.hissenet.utils;
+
+import com.infina.hissenet.entity.base.BaseEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class GenericServiceImpl<T extends BaseEntity, ID> implements IGenericService<T, ID> {
+
+    private final JpaRepository<T, ID> repository;
+
+    public GenericServiceImpl(JpaRepository<T, ID> repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public T save(T entity) {
+        return repository.save(entity);
+    }
+
+
+    @Override
+    public T update(T entity) {
+        return repository.save(entity);
+    }
+
+    @Override
+    public void delete(T entity) {
+        entity.setDeleted(true);
+        update(entity);
+    }
+
+    @Override
+    public void deleteById(ID id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public Optional<T> findById(ID id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<T> findAll() {
+        return repository.findAll();
+    }
+}

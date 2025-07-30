4d9c7b3c850ceeac1566a634e2236d5dfa35a4e6
berk cirak <atalayberkcrak@gmail.com>
2025-07-30 14:54:11 +0300

    feat: added generic methods to WalletService

--- backend/hissenet/src/main/java/com/infina/hissenet/service/WalletService.java
+++ backend/hissenet/src/main/java/com/infina/hissenet/service/WalletService.java
@@ -1,10 +1,17 @@
 package com.infina.hissenet.service;
 
+import com.infina.hissenet.entity.Wallet;
 import com.infina.hissenet.repository.WalletRepository;
+import com.infina.hissenet.utils.IGenericService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
-public class WalletService {
+@Transactional
+public class WalletService implements IGenericService<Wallet, Long> {
 
     private WalletRepository walletRepository;
 
@@ -13,6 +20,34 @@ public class WalletService {
     }
 
 
+    @Override
+    public Wallet save(Wallet entity) {
+        return walletRepository.save(entity);
+    }
 
+    @Override
+    public Wallet update(Wallet entity) {
+        return walletRepository.save(entity);
+    }
 
+    @Override
+    public void delete(Wallet entity) {
+        entity.setDeleted(true);
+        update(entity);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        walletRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Wallet> findById(Long id) {
+        return walletRepository.findById(id);
+    }
+
+    @Override
+    public List<Wallet> findAll() {
+        return walletRepository.findAll();
+    }
 }

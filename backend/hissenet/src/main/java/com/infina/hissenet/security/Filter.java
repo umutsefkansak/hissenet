c1e967a1daed19e84a6877c71008f27d47db1a4b
furkan.can <furkan.can@localhost.local>
2025-07-31 20:00:06 +0300

    feat: add custom authentication filter using OncePerRequestFilter

--- backend/hissenet/src/main/java/com/infina/hissenet/security/Filter.java
+++ backend/hissenet/src/main/java/com/infina/hissenet/security/Filter.java
@@ -1,4 +1,48 @@
 package com.infina.hissenet.security;
 
-public class Filter {
+import com.infina.hissenet.entity.Employee;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Component
+public class Filter extends OncePerRequestFilter{
+    private final JwtService jwtService;
+
+    public Filter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token=getToken(request);
+        // validate token ederken user var mı yok mu kontrol ediyorum
+        if(token!=null && jwtService.validateToken(token)) {
+            Employee user=jwtService.getUser(token);
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+        filterChain.doFilter(request, response);
+    }
+    private String getToken(HttpServletRequest request){
+        var cookies = request.getCookies();
+        if(cookies != null){
+            for (Cookie cookie : cookies){
+                if(cookie.getName().equals("login-token") && cookie.getValue() != null && !cookie.getValue().isEmpty()) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
 }
--- backend/hissenet/src/main/java/com/infina/hissenet/security/WebSecurityConfig.java
+++ backend/hissenet/src/main/java/com/infina/hissenet/security/WebSecurityConfig.java
@@ -8,19 +8,31 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
+    private final Filter filter;
+
+    public WebSecurityConfig(Filter filter) {
+        this.filter = filter;
+    }
+
     @Bean
     SecurityFilterChain webSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((a) -> a.
                         requestMatchers("api/**", "/swagger-ui/**",
                                 "/v3/api-docs/**"
                         ).permitAll().anyRequest().authenticated()
-                ).csrf(AbstractHttpConfigurer::disable)
+                )
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+
+                .csrf(AbstractHttpConfigurer::disable)
                 .headers(AbstractHttpConfigurer::disable)
                 .sessionManagement(x -> x.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(((request, response, authException) -> {
                     response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
                     response.setContentType("application/json");

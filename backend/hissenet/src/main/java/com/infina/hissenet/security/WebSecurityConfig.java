cfdf9b476a10306d63d0a67152ba0663155091f3
furkan.can <furkan.can@localhost.local>
2025-08-01 14:22:23 +0300

    chore: reset merge and prepare for new commit

--- backend/hissenet/src/main/java/com/infina/hissenet/security/WebSecurityConfig.java
+++ backend/hissenet/src/main/java/com/infina/hissenet/security/WebSecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
+
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {

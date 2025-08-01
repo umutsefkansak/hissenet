fe3e885a62540aff6d64ece73ce911b0b423e3ae
furkan.can <furkan.can@localhost.local>
2025-08-01 13:02:33 +0300

    feat(auth): hide sessionId field from JSON response

--- backend/hissenet/src/main/java/com/infina/hissenet/dto/response/AuthResponse.java
+++ backend/hissenet/src/main/java/com/infina/hissenet/dto/response/AuthResponse.java
@@ -1,8 +1,12 @@
 package com.infina.hissenet.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public record AuthResponse(
        EmployeeResponse employee,
+       @JsonIgnore
        String sessionId,
+       @JsonIgnore
        long time
 ) {
 }

5d55331a9db92fdc7e065495e3aff5b356c6a0cb
ademhilmibozkurt <bozkurtadem7@gmail.com>
2025-08-01 15:26:20 +0300

    feat:Preprocess
    -Text reader added. -Question and answer splitter addded. -Json file exporter added.

---
+++ chatbot/Preprocessing/data.py
@@ -0,0 +1,31 @@
+import uuid
+import json
+
+PATH: str = "<path>"
+
+def readTxt() -> str:
+    qaData: list = []
+
+    file      = open(f"{PATH}steps.txt", encoding="utf-8")
+    text: str = file.read()
+
+    textSplitted: list = text.split("\n")
+
+    for i in range(len(textSplitted)):
+        id       = str(uuid.uuid4())
+        question = textSplitted[i].split(":")[0]
+        answer   = textSplitted[i].split(":")[-1]
+        
+        qaData.append({"id": id, "question": question, "answer": answer})
+        
+    file.close
+    print(qaData)
+    return qaData
+    
+def createJsonFile(qaData: dict) -> None:
+    with open(f"{PATH}steps.json", "w", encoding="utf-8") as file:
+        json.dump(qaData, file, indent=4, ensure_ascii=False)
+
+qaData: str = readTxt()
+createJsonFile(qaData=qaData)
+

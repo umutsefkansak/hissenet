20cff85d86e5e76521902194e6ef3cff3d03fd86
berk cirak <atalayberkcrak@gmail.com>
2025-08-02 21:06:28 +0300

    refactor: some TransactionTypes removed

--- backend/hissenet/src/main/java/com/infina/hissenet/entity/enums/TransactionType.java
+++ backend/hissenet/src/main/java/com/infina/hissenet/entity/enums/TransactionType.java
@@ -4,13 +4,7 @@ public enum TransactionType {
 
         DEPOSIT,
         WITHDRAWAL,
-        TRANSFER,
         STOCK_PURCHASE,
         STOCK_SALE,
-        DIVIDEND,
         FEE,
-        REFUND,
-        BONUS,
-        PENALTY,
-        INTEREST
 }

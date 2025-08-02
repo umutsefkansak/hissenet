6a0937c0b24ba21b66fda7899fc2d1ac393e525c
umutsefkansak <umut.sefkan4@gmail.com>
2025-08-02 19:39:56 +0300

    refactor: update NotFoundException-based classes for consistency

--- backend/hissenet/src/main/java/com/infina/hissenet/exception/customer/CustomerNotFoundException.java
+++ backend/hissenet/src/main/java/com/infina/hissenet/exception/customer/CustomerNotFoundException.java
@@ -1,9 +1,13 @@
 package com.infina.hissenet.exception.customer;
 
 import com.infina.hissenet.exception.common.NotFoundException;
-
 public class CustomerNotFoundException extends NotFoundException {
     public CustomerNotFoundException(Long id) {
         super("Customer not found with id: " + id);
+    }
+
+
+    public CustomerNotFoundException(String message) {
+        super(message);
     }
 }

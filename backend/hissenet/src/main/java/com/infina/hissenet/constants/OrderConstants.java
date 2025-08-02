ea8baf8fd5f43b44f1bca06149e02854b14c199b
hakan-atalay <hakannatalayyy@gmail.com>
2025-08-03 00:30:32 +0300

    refactor: decouple stock price retrieval from database and use cache instead

---
+++ backend/hissenet/src/main/java/com/infina/hissenet/constants/OrderConstants.java
@@ -0,0 +1,11 @@
+package com.infina.hissenet.constants;
+
+import java.math.BigDecimal;
+
+public class OrderConstants {
+	
+	private OrderConstants() {}
+	
+	public static final BigDecimal COMMISSION_RATE = new BigDecimal("0.002");
+
+}

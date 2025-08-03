df1952624c660f13c01f3c5dd2a775e03619149f
ZG <zeynepguney@ZG-MacBook-Air.local>
2025-08-03 17:02:07 +0300

    embedding.py dosyasındaki düzenlemeler yapıldı

--- chatbot/Embed/embedding.py
+++ chatbot/Embed/embedding.py
@@ -1,7 +1,10 @@
 import json
+from dotenv import dotenv_values
 from sentence_transformers import SentenceTransformer
 
-PATH: str = "<path>"
+secrets = dotenv_values(".SECRETS")
+
+PATH: str = secrets["pathPREPROCESS"]
 
 def getJson() -> json:
     with open(f'{PATH}steps.json', 'r', encoding="utf-8") as file:
@@ -14,13 +17,14 @@ def exportJson(data: json) -> None:
 def getEmbeddingModel():
     # model = AutoModel.from_pretrained(f'{PATH}all-MiniLM-L6-v2')
     # model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-    model = SentenceTransformer(f'{PATH}all-MiniLM-L6-v2')
+    # model = SentenceTransformer(f'{PATH}all-MiniLM-L6-v2')
+    model = SentenceTransformer(f'{PATH}mxbai-embed-large-v1-nli-matryoshka')
     return model
 
-def makeEmbedding(text: str, model) -> []:
+def makeEmbedding(text: str, model) -> list:
     return model.encode(text) 
 
-def embedQandA(data: json, model) -> None:
+def embedQandA(data: json, model) -> json:
     for i in data:
         question: str = i['question'] 
         answer: str   = i['answer'] 
@@ -28,10 +32,12 @@ def embedQandA(data: json, model) -> None:
         qEmbedding = makeEmbedding(question, model)
         aEmbedding = makeEmbedding(answer, model)
         
-        i.update({"qEmbedding":str(qEmbedding)})
-        i.update({"aEmbedding":str(aEmbedding)})
+        i.update({"qEmbedding":qEmbedding.tolist()})
+        i.update({"aEmbedding":aEmbedding.tolist()})
 
-data: json       = getJson()
-model: AutoModel = getEmbeddingModel()
+    return data
+
+data: json = getJson()
+model      = getEmbeddingModel()
 embedQandA(data=data, model=model)
 exportJson(data)

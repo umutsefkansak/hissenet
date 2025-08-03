0347b1d77b17624e9518abcebd9f7ff4516ac3aa
umutsefkansak <umut.sefkan4@gmail.com>
2025-08-03 23:16:08 +0300

    refactor: Remove id field from RiskQuestionResponse and RoleResponse DTOs for data privacy

--- backend/hissenet/src/main/java/com/infina/hissenet/dto/response/RiskQuestionResponse.java
+++ backend/hissenet/src/main/java/com/infina/hissenet/dto/response/RiskQuestionResponse.java
@@ -3,7 +3,6 @@ package com.infina.hissenet.dto.response;
 import java.util.List;
 
 public record RiskQuestionResponse(
-        Long id,
         String questionText,
         List<RiskOptionResponse> options
 ) {}

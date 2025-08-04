9aa6154ced1db3bc728f9c358a736a797221c775
berk cirak <atalayberkcrak@gmail.com>
2025-08-04 15:03:07 +0300

    fix: fixed import problems

--- frontend/src/pages/ForgotPassword/ForgotPassword.js
+++ frontend/src/pages/ForgotPassword/ForgotPassword.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { sendVerificationCode } from '../server/api';
+import { sendVerificationCode } from '../../server/api';
 import './ForgotPassword.css';
 
 const ForgotPassword = () => {

2b37e683814ee7c683fc3d5e4c2f3d88003b3abf
berk cirak <atalayberkcrak@gmail.com>
2025-08-04 14:34:54 +0300

    feat: added new index.js docs and directories

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Layout/Sidebar/Navbar';
-import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
-import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
-import VerificationCode from './pages/VerificationCode/VerificationCode';
-import NewPassword from './pages/NewPassword/NewPassword';
+import Home from './pages/Home';                   
+import Login from './pages/Login';                  
+import ForgotPassword from './pages/ForgotPassword'; 
+import VerificationCode from './pages/VerificationCode'; 
+import NewPassword from './pages/NewPassword';      
+import Wallet from './pages/Wallet';                 
 import ToastContainer from './components/Toast/ToastContainer';
-import Wallet from './pages/Wallet/Wallet';
+
 import './App.css';
 
 function AppContent() {

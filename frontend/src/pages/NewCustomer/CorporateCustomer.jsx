237d5000ee090072618bea1dcd41660e57918622
umutsefkansak <umut.sefkan4@gmail.com>
2025-08-05 15:13:18 +0300

    feat(ui): add new customer registration pages (corporate & individual)

---
+++ frontend/src/pages/NewCustomer/CorporateCustomer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NewCustomer.css';
+
+const CorporateCustomer = () => {
+    return (
+        <div className="new-customer">
+            <div className="page-header">
+                <h1>Kurumsal Müşteri</h1>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default CorporateCustomer;

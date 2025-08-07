73fea1828d03e081a59c60a2d7be05f8e1c8141b
umutsefkansak <umut.sefkan4@gmail.com>
2025-08-08 01:34:43 +0300

    feat: update DTOs to support sending verification code by TC or tax number

--- backend/hissenet/src/main/java/com/infina/hissenet/dto/request/CustomerIdentificationRequest.java
+++ backend/hissenet/src/main/java/com/infina/hissenet/dto/request/CustomerIdentificationRequest.java
@@ -1,2 +1,8 @@
-package com.infina.hissenet.dto.request;public class CustomerIdentificationRequest {
-}
+package com.infina.hissenet.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CustomerIdentificationRequest(
+        @NotBlank @Size(min = 10, max = 11) String identificationNumber
+) {}
